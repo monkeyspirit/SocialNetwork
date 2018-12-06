937cdd783db8fa07cb542423e86d758300a38eb0
msalers <mariasaleri@gmail.com>
2018-12-06 22:44:16 +0100

    Domanda Enum

--- src/versione1/AgeGroup.java
+++ src/versione1/AgeGroup.java
@@ -1,6 +1,9 @@
 package versione1;
 
 /* fasce d'età, potremmo anche usare una libreria esterna */
+/* Maria: invece che creare esternamente l'enum potremmo anche crearlo direttamente la classe EventSoccerMatch?
+    In modo da aver il codice più compatto visto che sono enum tipici di quella classe, fatemi sapere se può avere senso.
+ */
 public enum AgeGroup {
 
 }
--- src/versione1/Gender.java
+++ src/versione1/Gender.java
@@ -1,5 +1,8 @@
 package versione1;
 
+/* Maria: invece che creare esternamente l'enum potremmo anche crearlo direttamente la classe EventSoccerMatch?
+    In modo da aver il codice più compatto visto che sono enum tipici di quella classe, fatemi sapere se può avere senso.
+ */
 public enum Gender {
     MALE, FEMALE, GENITORE1, GENITORE2
 }
