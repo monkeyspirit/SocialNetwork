70b77e9b4660a03b2460caf3bbf93e5639a9f88f
msalers <mariasaleri@gmail.com>
2018-12-13 18:59:34 +0100

    Prove con MouseListener

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,41 +9,46 @@ import versione1.EventSoccerMatch;
 import versione1.SoccerMatch;
 import versione1.SocialNetwork;
 
-import javax.swing.*;
-import java.io.IOException;
-
 public class Main extends Application {
 
 
-
+    /**
+     * Il metodo start ha il compito principale di far avviare l'interfaccia grafica e impostare
+     * tutti i parametri necessari al suo funzionamento, nel nostro caso si occupa anche di creare le istnze iniziali (in mancanza
+     * di un metodo di lettur scrittura da file)
+     * @param primaryStage
+     * @throws Exception
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        // Carico il file per la grafica
         FXMLLoader loader = new FXMLLoader(Main.class.getResource("sample.fxml"));
         SampleController controller = new SampleController();
 
+        // Creo le istanze principali del mio programma --> quando inseriremo il metodo di lettura
+        // e scrittura da file qui ci vorra una struttura di controllo come un case o un if
+        // e queste righe saranno in una parte di codice alternativa
         SocialNetwork social = new SocialNetwork();
         SoccerMatch soccer_match = new SoccerMatch();
         EventSoccerMatch eventSoccerMatch = new EventSoccerMatch();
         soccer_match.addEvent(eventSoccerMatch);
         social.addCategory(soccer_match);
 
+
+        // Imposto il controller
         loader.setController(controller);
 
+        // Passo a controller il riferimento a social network
         controller.setSocialNetwork(social);
 
-        System.out.println(social.getCategories().get(0).getName());
-
+        // Imposto lo stage e la scene principali
         Parent root =  (Parent) loader.load();
         Scene scene = new Scene(root, 600, 400);
         primaryStage.setTitle("Bacheca");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
-    /**
-     * Metodo per inizializzare i componenti base del progetto
-     */
 
 
     public static void main(String[] args) {
