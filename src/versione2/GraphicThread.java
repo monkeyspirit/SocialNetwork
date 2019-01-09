71b7cf01988a27e791b76b85aaaa9c572bf7594e
msalers <mariasaleri@gmail.com>
2019-01-09 11:59:53 +0100

    Introduzione thread per l'aggiornamento della grafica e metodo "rimuovi notifica"

---
+++ src/versione2/GraphicThread.java
@@ -0,0 +1,56 @@
+package versione2;
+
+
+import javafx.scene.control.ListView;
+import sample.SampleController;
+
+
+public class GraphicThread extends Thread {
+
+    // Attributi & Setter
+    private SampleController sampleController;
+
+    public void setSampleController(SampleController sampleController) { this.sampleController = sampleController; }
+
+    private ListView notificationListView,categoryListView, eventListView, userEventListView;
+
+    public void setNotificationListView(ListView notificationListView) { this.notificationListView = notificationListView; }
+
+    public void setCategoryListView(ListView categoryListView) { this.categoryListView = categoryListView; }
+
+    public void setEventListView(ListView eventListView) { this.eventListView = eventListView; }
+
+    public void setUserEventListView(ListView userEventListView) { this.userEventListView = userEventListView; }
+
+    @Override
+    public void run() {
+
+
+            while (true) {
+
+                sampleController.refreshListView();
+
+                categoryListView.refresh();
+                if(!categoryListView.getSelectionModel().isEmpty()){
+                    eventListView.refresh();
+                }
+
+                userEventListView.refresh();
+                System.out.println(userEventListView.getItems());
+                notificationListView.refresh();
+                System.out.println(notificationListView);
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+
+
+    }
+
+}
+
